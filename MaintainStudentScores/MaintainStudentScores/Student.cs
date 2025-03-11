214db9d63b5d526e885e27d1c774a96a63d60b32
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-03-11 10:44:00 -0600

    added student class

---
+++ MaintainStudentScores/MaintainStudentScores/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace MaintainStudentScores;
+
+public class Student
+{
+    string firstName { get; set; }
+    string lastName { get; set; }
+    List<Decimal> score = new List<Decimal>();
+}
