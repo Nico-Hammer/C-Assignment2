ccc69f63810d1ee293fb35287ceca6d901091fde
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-03-13 18:32:45 -0600

    commenting and validation for AddNewStudent form

--- MaintainStudentScores/MaintainStudentScores/Student.cs
+++ MaintainStudentScores/MaintainStudentScores/Student.cs
@@ -1,11 +1,13 @@
 ﻿namespace MaintainStudentScores;
 
+/* student class */
 public class Student
 {
-    public string firstName { get; set; }
-    public string lastName { get; set; }
-    public List<Decimal> score {get; set;}
+    public string firstName { get; set; } // students first name
+    public string lastName { get; set; } // students last name
+    public List<Decimal> score {get; set;} // list of students scores
 
+    /* student object constructor */
     public Student(string firstName, string lastName, List<decimal> score)
     {
         this.firstName = firstName;
